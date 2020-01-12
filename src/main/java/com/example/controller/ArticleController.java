dc0ec5550811c2c811ae9da6d375e8c07e910ec6
IidaShuhei <shuhei.iida@rakus-partners.co.jp>
2020-01-12 19:51:18 +0900

    Review画面

--- src/main/java/com/example/controller/ArticleController.java
+++ src/main/java/com/example/controller/ArticleController.java
@@ -41,13 +41,13 @@ public class ArticleController {
 
 	@Autowired
 	private ArticleService articleService;
-	
+
 	@Autowired
 	private CommentService commentService;
-	
+
 	@Autowired
 	private ArticleDetailService detailService;
-	
+
 	@ModelAttribute
 	private ArticleForm setUpForm() {
 		return new ArticleForm();
@@ -69,9 +69,7 @@ public class ArticleController {
 //		model.addAttribute("articleList", articleList);
 //		return "forward:/";
 //	}
-	
-	
-	
+
 	/**
 	 * 記事を全件検索する.
 	 * 
@@ -82,16 +80,18 @@ public class ArticleController {
 	public String findAll(Model model) {
 		List<Article> articleList = articleService.findAll();
 		model.addAttribute("articleList", articleList);
-		
+
 		// オートコンプリート用にJavaScriptの配列の中身を文字列で作ってスコープへ格納
-		StringBuilder articleListForAutocomplete = articleService.getArticleListForAutocomplete(articleService.findAll());
+		StringBuilder articleListForAutocomplete = articleService
+				.getArticleListForAutocomplete(articleService.findAll());
 		model.addAttribute("articleListForAutocomplete", articleListForAutocomplete);
-		
+
 		return "article_list";
 	}
-	
+
 	/**
 	 * 記事をタイトル、投稿者名、内容のいずれかで曖昧検索する
+	 * 
 	 * @param title
 	 * @param name
 	 * @param content
@@ -99,18 +99,18 @@ public class ArticleController {
 	 * @return
 	 */
 	@RequestMapping("/findByInfo")
-	public String findByTitleOrNameOrContent(SearchForm searchForm,Model model) {
+	public String findByTitleOrNameOrContent(SearchForm searchForm, Model model) {
 		List<Article> articleList = null;
-		if(searchForm.getSearch() == 1) {
+		if (searchForm.getSearch() == 1) {
 			articleList = articleService.showArticleListFindByTitle(searchForm.getContents());
-		} else if(searchForm.getSearch() == 2) {
+		} else if (searchForm.getSearch() == 2) {
 			articleList = articleService.showArticleListFindByName(searchForm.getContents());
-		} else if(searchForm.getSearch() == 3) {
+		} else if (searchForm.getSearch() == 3) {
 			articleList = articleService.showArticleListFindByContent(searchForm.getContents());
 		}
-		model.addAttribute("articleList",articleList);
+		model.addAttribute("articleList", articleList);
 		return "article_list";
-		
+
 	}
 
 	/**
@@ -126,7 +126,7 @@ public class ArticleController {
 		model.addAttribute("articleList", articleList);
 		return "article_list";
 	}
-	
+
 	/**
 	 * 記事を投稿者名から曖昧検索する.
 	 * 
@@ -140,7 +140,7 @@ public class ArticleController {
 		model.addAttribute("articleList", articleList);
 		return "article_list";
 	}
-	
+
 	/**
 	 * 記事を内容から曖昧検索する.
 	 * 
@@ -154,7 +154,7 @@ public class ArticleController {
 		model.addAttribute("articleList", articleList);
 		return "article_list";
 	}
-	
+
 	/**
 	 * 記事を投稿する画面.
 	 * 
@@ -164,23 +164,24 @@ public class ArticleController {
 	public String insert() {
 		return "insertArticle";
 	}
-	
+
 	/**
 	 * 記事を投稿する.
 	 * 
 	 * @param articleForm 記事フォーム
-	 * @param model モデル
-	 * @return　記事一覧
-	 * @throws IOException 
-	 * @throws ParseException 
+	 * @param model       モデル
+	 * @return 記事一覧
+	 * @throws IOException
+	 * @throws ParseException
 	 */
 	@RequestMapping("/registerArticle")
-	public String RegisterArticle(@Validated ArticleForm articleForm,BindingResult result,Model model) throws IOException, ParseException{
-		
-		if(result.hasErrors()) {
+	public String RegisterArticle(@Validated ArticleForm articleForm, BindingResult result)
+			throws IOException, ParseException {
+
+		if (result.hasErrors()) {
 			return insert();
 		}
-		
+
 		Article article = new Article();
 		article.setTitle(articleForm.getTitle());
 		article.setName(articleForm.getName());
@@ -188,41 +189,33 @@ public class ArticleController {
 		article.setContent(articleForm.getContent());
 		LocalDate localDate = LocalDate.now();
 		Date date = Date.from(localDate.atStartOfDay(ZoneId.systemDefault()).toInstant());
-		System.err.println(date);
 		article.setPostDate(date);
-		System.out.println("あいうえお" + articleForm);
-	System.err.println(articleForm.getTripStartDate());
-	System.err.println(articleForm.getTripEndDate());
-//	article.setTripStartDate(java.sql.Date.valueOf(articleForm.getTripStartDate()));
-//    article.setTripEndDate(java.sql.Date.valueOf(articleForm.getTripEndDate()));
-	System.err.println(articleForm.getTripStartDate());
-	SimpleDateFormat sdf = new SimpleDateFormat("yyyy/mm/dd");
-	Date startDate = sdf.parse(articleForm.getTripStartDate());
-	article.setTripStartDate(startDate);
-	
-	Date endDate = sdf.parse(articleForm.getTripEndDate());
-	article.setTripEndDate(endDate);
-		
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy/mm/dd");
+		Date startDate = sdf.parse(articleForm.getTripStartDate());
+		article.setTripStartDate(startDate);
+
+		Date endDate = sdf.parse(articleForm.getTripEndDate());
+		article.setTripEndDate(endDate);
+
 		byte[] encoded = Base64.getEncoder().encode(articleForm.getImagePath().getBytes());
 		Charset charset = StandardCharsets.UTF_8;
-		
+
 		String base64 = new String(encoded, charset);
-		String fileExtension = articleForm.getImagePath().getOriginalFilename().substring(articleForm.getImagePath().getOriginalFilename().length() - 3);
+		String fileExtension = articleForm.getImagePath().getOriginalFilename()
+				.substring(articleForm.getImagePath().getOriginalFilename().length() - 3);
 		StringBuilder base64image = new StringBuilder();
-		if("jpg".equals(fileExtension)) {
+		if ("jpg".equals(fileExtension)) {
 			base64image.append("data:image/jpeg;base64,");
 		}
-		if("png".equals(fileExtension)) {
+		if ("png".equals(fileExtension)) {
 			base64image.append("data:image/png;base64,");
 		}
 		base64image.append(base64);
 		article.setImagePath(base64image.toString());
-		
 		articleService.registerArticle(article);
-	    
-	    return "forward:/";
+		return "forward:/";
 	}
-	
+
 	/**
 	 * コメントを挿入する.
 	 * 
@@ -237,57 +230,58 @@ public class ArticleController {
 		commentService.insert(comment);
 		return "redirect:/";
 	}
-	
+
 	/**
 	 * 更新画面へ遷移.
 	 * 
-	 * @return　更新画面
+	 * @return 更新画面
 	 */
 	@RequestMapping("/updateScreen")
-	public String updateScreen(Integer id,Model model, ArticleForm articleForm) {
+	public String updateScreen(Integer id, Model model, ArticleForm articleForm) {
 		Article article = detailService.showArticleDetail(id);
 		BeanUtils.copyProperties(article, articleForm);
 		model.addAttribute("article", article);
 		return "updateArticle";
 	}
-	
+
 	/**
 	 * 記事を更新する.
 	 * 
 	 * @param articleForm 記事フォーム
-	 * @param model　モデル
-	 * @return　編集画面
-	 * @throws ParseException 
-	 * @throws IOException 
+	 * @param model       モデル
+	 * @return 編集画面
+	 * @throws ParseException
+	 * @throws IOException
 	 */
 	@RequestMapping("/update")
-	public String update(ArticleForm articleForm,Model model) throws ParseException, IOException {
+	public String update(ArticleForm articleForm, Model model) throws ParseException, IOException {
 		Article article = new Article();
 		BeanUtils.copyProperties(articleForm, article);
 		article.setId(Integer.parseInt(articleForm.getId()));
 		LocalDate localDate = LocalDate.now();
 		Date date = Date.from(localDate.atStartOfDay(ZoneId.systemDefault()).toInstant());
 		article.setPostDate(date);
-		
+
 		byte[] encoded = Base64.getEncoder().encode(articleForm.getImagePath().getBytes());
 		Charset charset = StandardCharsets.UTF_8;
-		
+
 		String base64 = new String(encoded, charset);
-		String fileExtension = articleForm.getImagePath().getOriginalFilename().substring(articleForm.getImagePath().getOriginalFilename().length() - 3);
+		String fileExtension = articleForm.getImagePath().getOriginalFilename()
+				.substring(articleForm.getImagePath().getOriginalFilename().length() - 3);
 		StringBuilder base64image = new StringBuilder();
-		if("jpg".equals(fileExtension)) {
+		if ("jpg".equals(fileExtension)) {
 			base64image.append("data:image/jpeg;base64,");
 		}
-		if("png".equals(fileExtension)) {
+		if ("png".equals(fileExtension)) {
 			base64image.append("data:image/png;base64,");
 		}
 		base64image.append(base64);
 		article.setImagePath(base64image.toString());
-		
+
 		articleService.update(article);
 		return "forward:/";
 	}
-	
+
 	/**
 	 * 記事を削除する.
 	 * 
@@ -299,7 +293,7 @@ public class ArticleController {
 		articleService.delete(id);
 		return "redirect:/";
 	}
-	
+
 	@RequestMapping("/prefecture")
 	public String index2() {
 		return "japan";
